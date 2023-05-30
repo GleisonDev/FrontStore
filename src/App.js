23911ebaff5ef415f93453330b5766011c9f161d
Gleison <gleisondev5690@gmail.com>
2023-05-30 11:27:25 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import Formulario from './formulario';
+import Tabela from './Tabela';
+
 
 function App() {
+
+  // Objeto produto
+  const produto = {
+    id: 0,
+    name: '',
+    brand: ''
+  }
+
+  // UserState
+  const [btnCadastrar, setBtnCadastrar] = useState(true);
+  const [produtos, setProdutos] = useState([]);
+  const [objProduto, setObjProduto] = useState(produto);
+  //UserEffect
+  useEffect(() => {
+    fetch("http://localhost:8080/list")
+      .then(retorno => retorno.json())
+      .then(retorno_convertido => setProdutos(retorno_convertido))
+  }, []);
+
+  // Obtendo os dados do formulário
+  const aoDigitar = (e) => {
+    setObjProduto({ ...objProduto, [e.target.name]: e.target.value });
+  }
+
+  // Cadastrar produtos
+  const cadastrar = () => {
+    fetch('http://localhost:8080/register', {
+      method: 'post',
+      body: JSON.stringify(objProduto),
+      headers: {
+        'Content-type': 'application/json',
+        'Accept': 'application/json'
+
+      }
+    })
+      .then(retorno => retorno.json())
+      .then(retorno_convertido => {
+        console.log(retorno_convertido)
+      })
+  }
+  // Retorno
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Formulario botao={btnCadastrar} eventoTeclado={aoDigitar} cadastrar={cadastrar} />
+      <Tabela vetor={produtos} />
     </div>
   );
 }
---
+++ src/formulario.js
@@ -0,0 +1,25 @@
+function Formulario({ botao, eventoTeclado, cadastrar }) {
+    return (
+        <form>
+            <input type='text' onChange={eventoTeclado} name='name' placeholder='Nome' className='form-control' />
+            <input type='text' onChange={eventoTeclado} name='brand' placeholder='Marca' className='form-control' />
+
+            {
+
+                botao
+                    ?
+                    <input type='button' value='Cadastrar' onClick={cadastrar} className='btn btn-primary' />
+                    :
+                    <div>
+                        <input type='button' value='Alterar' className='btn btn-warning' />
+                        <input type='button' value='Remover' className='btn btn-danger' />
+                        <input type='button' value='Cancelar' className='btn btn-secondary' />
+                    </div>
+            }
+
+
+        </form>
+    )
+}
+
+export default Formulario;
